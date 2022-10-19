68747b6ed95d2c53e7e4119f47ea101c2233408f
Kjell Ohlsson <kjell.ohlsson@astrazeneca.com>
2022-10-19 15:42:41 +0200

    added tests of standard-deviation

--- tests/test_models.py
+++ tests/test_models.py
@@ -31,7 +31,7 @@ def test_daily_mean(test_mean, expected_mean):
         ([[1, 2], [3, 4], [5, 6]], [1, 2]),
     ])
 def test_daily_min(test_min, expected_min):
-    """Test mean function works for array of zeroes and positive integers."""
+    """Test mean function works."""
     from inflammation.models import daily_min
     npt.assert_array_equal(daily_min(np.array(test_min)), np.array(expected_min))
 
@@ -42,7 +42,18 @@ def test_daily_min(test_min, expected_min):
         ([[1, 2], [3, 4], [5, 6]], [5, 6]),
     ])
 def test_daily_max(test_max, expected_max):
-    """Test mean function works for array of zeroes and positive integers."""
+    """Test mean function works."""
     from inflammation.models import daily_max
     npt.assert_array_equal(daily_max(np.array(test_max)), np.array(expected_max))
+
+@pytest.mark.parametrize(
+    "test_std, expected_std",
+    [
+        ([[7, 8], [9, 15], [11, 20]], [1.633, 4.922]),
+        ([[1, 2], [3, 7], [5, 19]], [1.633, 7.134]),
+    ])
+def test_daily_std(test_std, expected_std):
+    """Test standard deviation function works."""
+    from inflammation.models import daily_std
+    npt.assert_allclose(daily_std(np.array(test_std)), np.array(expected_std), rtol=1e-04)
 
